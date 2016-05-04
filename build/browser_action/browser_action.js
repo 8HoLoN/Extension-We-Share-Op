2fe37b6e63fc9c3d57d611ee6d015c7fbb2c8463
8HoLoN <8holon@gmail.com>
2016-05-04 09:48:52 +0200

    add build directory

---
+++ build/browser_action/browser_action.js
@@ -0,0 +1,2 @@
+"use strict";!function(){var e=chrome.extension.getBackgroundPage(),n=e.WeShareOp;n.browserAction(window,function(){}),document.querySelector("#wso-amundiAccess").addEventListener("click",function(){n.openNewAmundiTab()})}();
+//# sourceMappingURL=browser_action.js.map
