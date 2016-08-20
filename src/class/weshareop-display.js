b91b2b3667b1004202df1b346a4eee9012ba3e43
8HoLoN <8holon@gmail.com>
2016-08-20 13:13:34 +0200

    improve i18n maintainability

--- src/class/weshareop-display.js
+++ src/class/weshareop-display.js
@@ -12,54 +12,20 @@
 
 	WeShareOp.prototype.displayI18n = function(_window){
 		var _gEBI = _window.document.getElementById.bind(_window.document);
+		var _qSA = _window.document.querySelectorAll.bind(_window.document);
 		var _gM = chrome.i18n.getMessage;
 
-		_gEBI("wso-statsTabLabel").innerHTML = _gM("statsTabName");
-		_gEBI("wso-sharesValueLabel").innerHTML = _gM("sharesValueLabel");
-		_gEBI("wso-sharesValue").title = _gM("sharesValueTitle");
-		_gEBI("wso-investmentValueLabel").innerHTML = _gM("investmentValueLabel");
-		_gEBI("wso-investmentValue").title = _gM("investmentValueTitle");
-		_gEBI("wso-capitalGainLabel").innerHTML = _gM("capitalGainLabel");
-		_gEBI("wso-capitalGain").title = _gM("capitalGainTitle");
-		_gEBI("wso-overallGainLabel").innerHTML = _gM("overallGainLabel");
-		_gEBI("wso-overallGain").title = _gM("overallGainTitle");
+		var _i18n = ['innerHTML','title','placeholder'];
+		_i18n = _i18n.map((_v,_k)=>
+			[].concat(_v,_k?['-'+_v,_v[0].toUpperCase()+_v.slice(1)]:['',''])
+		);
 
-		_gEBI("wso-sharesTabLabel").innerHTML = _gM("sharesTabLabel");
-		_gEBI("wso-numberOfSharesPurchasedLabel").innerHTML = _gM("numberOfSharesPurchasedLabel");
-		_gEBI("wso-numberOfSharesPurchased").title = _gM("numberOfSharesPurchasedTitle");
-		_gEBI("wso-numberOfSharesAcquiredLabel").innerHTML = _gM("numberOfSharesAcquiredLabel");
-		_gEBI("wso-numberOfSharesAcquired").title = _gM("numberOfSharesAcquiredTitle");
-		_gEBI("wso-buyingPriceLabel").innerHTML = _gM("buyingPriceLabel");
-		_gEBI("wso-buyingPrice").title = _gM("buyingPriceTitle");
-		_gEBI("wso-amountOfInvestmentLabel").innerHTML = _gM("amountOfInvestmentLabel");
-		_gEBI("wso-amountOfInvestment").title = _gM("amountOfInvestmentTitle");
-
-		_gEBI("wso-optionsTabLabel").innerHTML = _gM("optionsTabLabel");
-		_gEBI("wso-showOverallGainOnBadgeLabel").innerHTML = _gM("showOverallGainOnBadgeLabel");
-
-		_gEBI("wso-amundiAccountTabLabel").innerHTML = _gM("amundiAccountTabLabel");
-		_gEBI("wso-amundiLoginLabel").innerHTML = _gM("amundiLoginLabel");
-		_gEBI("wso-amundiLogin").placeholder = _gM("amundiLoginPlaceholder");
-		_gEBI("wso-amundiPasswordLabel").innerHTML = _gM("amundiPasswordLabel");
-		_gEBI("wso-amundiPassword").placeholder = _gM("amundiPasswordPlaceholder");
-
-		_gEBI("wso-amundiAccess").innerHTML = _gM("amundiAccess");
-
-		_gEBI("wso-infoTabLabel").innerHTML = _gM("infoTabLabel");
-		_gEBI("wso-openingLabel").innerHTML = _gM("openingLabel");
-		_gEBI("wso-openingDays").innerHTML = _gM("openingDays");
-		_gEBI("wso-schedulesLabel").innerHTML = _gM("schedulesLabel");
-		_gEBI("wso-preopeningLabel").innerHTML = _gM("preopeningLabel");
-		_gEBI("wso-sessionLabel").innerHTML = _gM("sessionLabel");
-		_gEBI("wso-preclosingLabel").innerHTML = _gM("preclosingLabel");
-		_gEBI("wso-exceptLabel").innerHTML = _gM("exceptLabel");
-		_gEBI("wso-goodFriday").innerHTML = _gM("goodFriday");
-		_gEBI("wso-easterMonday").innerHTML = _gM("easterMonday");
-		_gEBI("wso-1stMay").innerHTML = _gM("1stMay");
-		_gEBI("wso-25thDecember").innerHTML = _gM("25thDecember");
-		_gEBI("wso-26thDecember").innerHTML = _gM("26thDecember");
-
-		_gEBI("wso-aboutTabLabel").innerHTML = _gM("aboutTabLabel");
+		_i18n.map((_v,_k)=>{
+			Array.from(_qSA('[data-i18n'+_v[1]+']')).map(_el =>{
+				_el[_v[0]] = _gM(_el.getAttribute('data-i18n'+_v[1]));
+				delete _el.dataset['i18n'+_v[2]];
+			});
+		});
 
 		//_gEBI("wso-feedback").href = "mailto:"+"8holon"+"@"+"gmail.com?Subject=Feedback";
 		//_gEBI("wso-feedback").innerHTML = "8holon"+"&#64;"+"gmail.com";
@@ -67,15 +33,6 @@
 		_gEBI("wso-feedback").innerHTML = "alexandre.remy.contact"+"&#64;"+"gmail.com";
 
 		_gEBI("wso-version").innerHTML = this.version;
-
-		_gEBI("wso-versionLabel").innerHTML = _gM("versionLabel");
-		_gEBI("wso-links").innerHTML = _gM("links");
-		_gEBI("wso-reviews").innerHTML = _gM("reviews");
-		_gEBI("wso-feedbackLabel").innerHTML = _gM("feedbackLabel");
-
-		_gEBI("wso-loading").innerHTML = _gM("loading");
-		_gEBI("wso-closeModal").innerHTML = _gM("closeModal");
-
 	};
 
 	WeShareOp.prototype.displayData = function(_window){
